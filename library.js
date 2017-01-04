498b341c118cd6c21fbc0213abc11ea15ed08501
subji <qkrwlrjq@gmail.com>
2017-01-04 17:28:46 +0900

    Modified library.js

--- library.js
+++ library.js
@@ -1,19 +1,37 @@
-'use strict';
+// 'use strict';
 
-var meta = module.parent.require('./meta');
-var user = module.parent.require('./user');
 
-var winston = require('winston');
 
-var plugin = {};
+// var plugin = {};
 
-plugin.init = function (params, callback)	{
-	winston.add(winston.transports.File, {
-		level: 'debug',
-		json: false,
-		filename: 'my.log'
-	});
+// plugin.init = function (params, callback)	{
+// 	winston.add(winston.transports.File, {
+// 		level: 'debug',
+// 		json: false,
+// 		filename: 'my.log'
+// 	});
 
-	winston.info('This is info');
-	winston.error('This is error');
-}
+// 	winston.info('This is info');
+// 	winston.error('This is error');
+// }
+
+(function (module)	{
+	'use strict';
+
+	var meta = module.parent.require('./meta');
+	var user = module.parent.require('./user');
+
+	var winston = require('winston');
+
+	var init = function (params, callback)	{
+		winston.add(winston.transports.File, {
+			level: 'debug',
+			json: false,
+			filename: 'my.log'
+		});
+
+		winston.info('This is info');
+		winston.error('This is error');		
+	}
+
+}(module));
