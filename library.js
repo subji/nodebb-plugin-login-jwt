46424797a04a0bffa51a816fc6b0f7ac50b92457
subji <qkrwlrjq@gmail.com>
2017-01-04 17:07:30 +0900

    Additional npm modules

--- library.js
+++ library.js
@@ -3,8 +3,17 @@
 var meta = module.parent.require('./meta');
 var user = module.parent.require('./user');
 
+var winston = require('winston');
+
 var plugin = {};
 
 plugin.init = function (params, callback)	{
-	console.log(arguments);
+	winston.add(winston.transports.File, {
+		level: 'debug',
+		json: false,
+		filename: 'my.log'
+	});
+
+	winston.info('This is info');
+	winston.error('This is error');
 }
