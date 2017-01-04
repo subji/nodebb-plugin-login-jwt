68ef07269e7bfffe0619dba97c80a04479411d9a
subji <qkrwlrjq@gmail.com>
2017-01-04 16:17:11 +0900

    Add package.json and plugin.json

--- library.js
+++ library.js
@@ -1,4 +1,10 @@
-(function (module)	{
-	'use strict';
+'use strict';
 
-}(module));
+var meta = module.parent.require('./meta');
+var user = module.parent.require('./user');
+
+var plugin = {};
+
+plugin.init = function (params, callback)	{
+	console.log(arguments);
+}
