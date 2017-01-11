61d2610a2b8c71b2c1a13a5a751c9ed4576c2703
subji <qkrwlrjq@gmail.com>
2017-01-11 15:05:49 +0900

    Exist user

--- library.js
+++ library.js
@@ -2,7 +2,9 @@
 
 var meta = module.parent.require('./meta');
 var user = module.parent.require('./user');
-var db   = module.parent.require('./database');
+
+var db   = module.parent.require('./database'); // NodeBB / src / database 참고.
+var au   = module.parent.require('./controllers/authentication'); // NodeBB / src / controllers 참고.
 
 var winston = require('winston');
 var jwt = require('jsonwebtoken');
@@ -33,12 +35,13 @@ plugin.loggedin = function (params, callback)	{
 
 plugin.addMiddleware = function (req, res, next)	{
 	var decoded = jwt.verify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3RAZ21haWwuY29tIiwiaXNMb2dpbiI6IlllcyJ9.L8r4Ibbp6N30VoTpRL_U3rouXdOK4IflGbeOOjlCAew', 'secret');
+	var isExist = db.getObjectField(decoded.username, 'username');
 
-	console.log(decoded);
+	// if (!isExist)	{
+	// 	db.setObjectField(decoded.username, 'username', )
+	// }
 
-	console.log(module.parent, '\n\n');
-
-	console.log(db, meta, user);
+	console.log(decoded, isExist);
 
 	next();
 };
